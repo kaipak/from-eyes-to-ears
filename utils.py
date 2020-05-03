f00d2529772409407267a3d46183b81f7b68aa06
Shaun Litt <slitt@conversantmedia.com>
2020-05-03 12:47:53 -0500

    update

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 from google.colab import files
 import os
 import glob
+import cv2
 
 def upload_file():
     print("Provide a test image")
-    test_img = files.upload()
+    img = files.upload()
     list_of_files = glob.glob('./*') # * means all if need specific format then *.csv
-    test_img = max(list_of_files, key=os.path.getctime)
-    return test_img
+    img_file = max(list_of_files, key=os.path.getctime)
+    img = cvs.imread(img_file)
+    return (img, img_file)
