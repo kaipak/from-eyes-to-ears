9fb9e08acbb3d37722a2dd5dfbe47d2c7275ec61
Shaun Litt <slitt@conversantmedia.com>
2020-05-04 15:46:34 -0500

    more bug fixes

--- blended_predictor.py
+++ blended_predictor.py
@@ -15,7 +15,7 @@ class BlendPredictor:
         self.mask_md = mask_metadata if mask_metadata is not None else MetadataCatalog.get(mask_config.DATASETS.TRAIN[0])
         self.shrink_threshold = shrink_threshold if shrink_threshold is not None else self.shrink_threshold
         self._merge_metadata()
-    
+
     def _merge_metadata(self):
         added_classes = self.mask_md.get('thing_classes')
         total_classes = self.po_md.get('thing_classes').copy()
@@ -33,18 +33,20 @@ class BlendPredictor:
 
     def predict(self, img):
         # First lets run the predictions
-        self.panoptic_seg, self.panoptic_seg_info = self.po_predictor(img)["panoptic_seg"] 
+        self.panoptic_seg, self.panoptic_seg_info = self.po_predictor(img)["panoptic_seg"]
         self.mask_output = self.mask_predictor(img)
-        return blend_segs(self.panoptic_seg, self.mask_output)
+        return self.blend_segs(self.panoptic_seg, self.panoptic_seg_info, self.mask_output)
 
-    def blend_segs(self, panoptic_seg, mask_output):
-        blend_seg = panoptic_seg.to("cpu").numpy().copy()
+    def blend_segs(self, panoptic_seg, panoptic_seg_info, mask_output):
+        total_classes = self.blend_md.get('thing_classes')
+        po_seg = panoptic_seg.to("cpu").numpy()
+        blend_seg = po_seg.copy()
         max_seg = blend_seg.max()
-        blend_info = segments_info.copy()
+        blend_info = panoptic_seg_info.copy()
 
-        masks = mask_outputs["instances"].to("cpu").get("pred_masks").numpy()
-        scores = mask_outputs["instances"].to("cpu").get("scores").numpy()
-        classes = mask_outputs["instances"].to("cpu").get("pred_classes").numpy()
+        masks = mask_output["instances"].to("cpu").get("pred_masks").numpy()
+        scores = mask_output["instances"].to("cpu").get("scores").numpy()
+        classes = mask_output["instances"].to("cpu").get("pred_classes").numpy()
 
         # basic blending
         new_seg_info = []
@@ -64,19 +66,22 @@ class BlendPredictor:
             blend_seg = blend_seg + (m * iid)
             area = np.count_nonzero(blend_seg == iid)
 
-            info = {"id": iid, "isthing": True, "score": s, "category_id": c + offset, "instance_id": instance_ids[c], "area": area}
+            info = {"id": iid, "isthing": True, "score": s, "category_id": c + self.offset, "instance_id": instance_ids[c], "area": area}
             blend_info.append(info)
-
+            
         # Remove "dead" instances where the instance has lost more than N% of volume
         final_info = []
         for i in range(len(blend_info)):
             seg = blend_info[i]
-            if i >= len(segments_info):
+            if i >= len(panoptic_seg_info):
                 final_info.append(seg)
                 continue
-            orig_seg = segments_info[i]
+            orig_seg = panoptic_seg_info[i]
             iid = seg["id"]
-            orig_area = orig_seg["area"]
+            if "area" in orig_seg:
+                orig_area = orig_seg["area"]
+            else:
+                orig_area = np.count_nonzero(po_seg==iid)
             new_area = np.count_nonzero(blend_seg==iid)
             pct = new_area/orig_area
             if pct > self.shrink_threshold:
@@ -89,4 +94,3 @@ class BlendPredictor:
         final_seg = torch.tensor(blend_seg)
 
         return final_seg, blend_info
-
